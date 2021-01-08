d6beaa786efd6bc129a4cdb55a81d73b18609053
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-01-08 15:19:56 -0300

    Added first call to api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recomendedProducts, setRecomendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response =>{
+      response.json().then(data => {
+        setRecomendedProducts(data);
+      });
+    });
+  },[])
+
   return (
    <div>
-     <Title>Hello Jhonatan</Title>
+     <section>
+      <Title>Products</Title>
+
+      <ul>
+        {recomendedProducts.map(recomendedProducts => {
+          return (
+            <li key={recomendedProducts.id}>
+              {recomendedProducts.title}
+            </li>
+          );
+        })}
+      </ul>
+     </section>
    </div>
   )
 }
