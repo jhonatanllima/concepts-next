1a7d2b3c3218a592a9aec1afa241a2044b17c78a
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-01-07 23:35:52 -0300

    Added styled components and configure to Server-Side-Rendering

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    background: #121214;
+    color: #fff;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
